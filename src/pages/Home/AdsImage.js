a2fbffc49ca5bb3c28ee73354925cc0f8905edaa
Quy Nguyen <quynguyen@tomochain.com>
2022-09-19 16:42:05 +0700

    update

--- src/pages/Home/AdsImage.js
+++ src/pages/Home/AdsImage.js
@@ -5,10 +5,14 @@ const Img = styled.img`
     width: 100%;
     height: auto;
     margin: 200px 0;
+    cursor: pointer;
 `
 
 const AdsImage = () => {
-return <Img src={`${process.env.PUBLIC_URL}/assets/images/image-ads.png`}/>
+    const onClick = () => {
+        window.open('https://forum.tomochain.com/t/luaswap-ama-homestead-planning-latest-progress-partnership-more/2453', '_blank', 'noopener,noreferrer');
+    }
+    return <Img onClick={onClick} src={`${process.env.PUBLIC_URL}/assets/images/image-ads.png`}/>
 }
 
 export default AdsImage;
