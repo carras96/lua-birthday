4badf1f2bb0b4799aa59d8e99c8253528b711de1
Quy Nguyen <quynguyen@tomochain.com>
2022-09-23 09:30:14 +0700

    update

--- src/pages/DailyHunt/Modals/StakeModal.js
+++ src/pages/DailyHunt/Modals/StakeModal.js
@@ -13,7 +13,7 @@ const StakeModal = ({ toggleModal }) => {
     };
     return <Wrapper>
         <ModalTitle>Stake</ModalTitle>
-        <ModalDesc>Stake at least 1,000 LUA in LuaSafe to be eligible</ModalDesc>
+        <ModalDesc>Stake at least 500 LUA in LuaSafe to be eligible</ModalDesc>
         <form onSubmit={handleSubmit(onSubmit)}>
             <InputTitle>Your Stake transaction</InputTitle>
             <InputModal placeholder='0x...' {...register("stake", { required: true, pattern: /^0x([A-Fa-f0-9]{64})$/ })} />
@@ -33,7 +33,7 @@ const StakeModal = ({ toggleModal }) => {
         <GuideDetail>
             <TextGuide>1. Go to <TextGreen href='https://app.luaswap.org/#/lua-safe' target='_blank'>LuaSafe</TextGreen> tab</TextGuide>
             <TextGuide>2. <b>Approve</b> the LUA token</TextGuide>
-            <TextGuide>3. Click <b>Stake</b> button and enter the number of LUA you want to stake (at least 1,000 LUA)</TextGuide>
+            <TextGuide>3. Click <b>Stake</b> button and enter the number of LUA you want to stake (at least 500 LUA)</TextGuide>
             <TextGuide>4. Click <b>Confirm</b></TextGuide>
             <TextGuide>5. Put the Tx Hash in the box above 👆</TextGuide>
         </GuideDetail>
